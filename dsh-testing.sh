1bad678bda28e7faef98c480346e483da4b1d6e3
garvit3835 <garvit3835@gmail.com>
2025-01-09 11:40:49 +0530

    updated helm commands

--- dsh-testing.sh
+++ dsh-testing.sh
@@ -5,7 +5,7 @@ set -e
 apply_terraform() {
   echo "Initializing Terraform..."
   if [[ "$TF_BACKEND" == "S3" ]]; then
-    terraform init -backend-config="bucket=${BUCKET_NAME}" \
+    terraform init -backend-config="bucket=dsh-testing-tf-state-2" \
                    -backend-config="key=devzero/terraform.tfstate" \
                    -backend-config="region=us-west-1"
   else
@@ -25,17 +25,17 @@ configure_kubeconfig() {
 }
 
 login_to_helm_registry() {
-  helm registry login registry.devzero.io --username $DZ_USERNAME --password $DZ_PASSWORD
+  docker login -u $DZ_USERNAME -p $DZ_PASSWORD
 }
 
 install_devzero_crds() {
-  helm install dz-control-plane-crds oci://registry.devzero.io/devzero-control-plane/beta/dz-control-plane-crds \
-    -n devzero --create-namespace
+  helm pull oci://registry-1.docker.io/devzeroinc/dz-data-plane-crds
+  helm install dz-control-plane-crds oci://registry-1.docker.io/devzeroinc/dz-control-plane-crds -n devzero --create-namespace
 }
 
 install_devzero_control_plane() {
-  helm install dz-control-plane oci://registry.devzero.io/devzero-control-plane/beta/dz-control-plane \
-    -n devzero --set domain=$DOMAIN_NAME --set issuer.email=$EMAIL
+  helm pull oci://registry-1.docker.io/devzeroinc/dz-control-plane
+  helm install dz-control-plane oci://registry-1.docker.io/devzeroinc/dz-control-plane -n devzero --set domain=dsh-testing.com --set issuer.email=support@devzero.io --set credentials.registry=docker.io/devzeroinc --set credentials.username=$HELM_USER --set credentials.password=$HELM_PSWD --set credentials.email=support@devzero.io
 }
 
 get_ingress_service() {
